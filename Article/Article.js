4b9c61483a8a5d30f0db145dc5f57e1c95d87337
Timothy Shores <timothymshores@gmail.com>
2019-01-30 16:42:57 -0600

    Attempted to add strech goal animaions using Animate.css

--- Article/Article.js
+++ Article/Article.js
@@ -12,12 +12,12 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    this.domElement.classList.toggle('slideInDown');
   }
 }
 
 // Select all classes named ".article" and assign that value to the articles variable.  
 let articles = document.querySelectorAll('.article');
-console.log(articles);
 
 // Iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles.forEach(article => new Article(article));
